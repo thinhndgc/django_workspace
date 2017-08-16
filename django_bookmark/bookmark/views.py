bed0d5748428831887b304819397ee2887e70dcc
thinh nguyen <ndthinh94@gmail.com>
2017-08-16 11:23:48 +0700

    fixed main page

--- django_bookmark/bookmark/views.py
+++ django_bookmark/bookmark/views.py
@@ -3,14 +3,18 @@ from django.http import HttpResponse, Http404
 from django.template import Context
 from django.template.loader import get_template
 from django.contrib.auth.models import User
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.contrib.auth.decorators import login_required
 # Create your views here.
-
+# @ensure_csrf_cookie
+# @login_required(login_url='/bookmark/login/')
 def main_page(request):
+    print(request.user.username)
     return render_to_response(
         'main_page.html',
-        {'user: request.user'}
+        {'user': request.user}
     )
-    
+
 def user_page(request, username):
     try:
         user = User.objects.get(username=username)
