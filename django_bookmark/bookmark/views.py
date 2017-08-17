558fcb3f315ce3dccb2aa6f4bb8695d51f313ded
= <ndthinh94@gmail.com>
2017-08-18 02:09:26 +0700

    edit some small things

--- django_bookmark/bookmark/views.py
+++ django_bookmark/bookmark/views.py
@@ -4,20 +4,21 @@ from django.template import Context
 from django.template.loader import get_template
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import ensure_csrf_cookie
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
 from bookmark.forms import *
 # Create your views here.
 # @ensure_csrf_cookie
-# @login_required(login_url='/bookmark/login/')
+
+
 def main_page(request):
     print(request.user.username)
     return render_to_response(
         'main_page.html',
         RequestContext(request)
     )
+
 
 def user_page(request, username):
     try:
@@ -34,15 +35,16 @@ def user_page(request, username):
     template = get_template('user_page.html')
     return render_to_response('user_page.html', variables)
 
+
 def register_page(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
             print(form.cleaned_data['user_name'])
             user = User.objects.create_user(
-                username = form.cleaned_data['user_name'],
-                password = form.cleaned_data['password1'],
-                email = form.cleaned_data['email']
+                username=form.cleaned_data['user_name'],
+                password=form.cleaned_data['password1'],
+                email=form.cleaned_data['email']
             )
             return HttpResponseRedirect('/bookmark/register/success/')
     else:
@@ -55,6 +57,7 @@ def register_page(request):
         'registration/register.html',
         variables
     )
+
 
 def log_out(request):
     logout(request)
